e633d2f7256ff412c6a5bb541172644902676179
adithyanajay@gmail.com <adithyanajay@gmail.com>
2024-01-20 18:15:43 +0530

    quizzess added

---
+++ src/quizzes/index.js
@@ -0,0 +1,47 @@
+export const quizzes = [
+  {
+    quiz: "Berlin is the capital of ",
+    options: [
+      { option: "A", text: "Germany", correct: true },
+      { option: "B", text: "France", correct: false },
+      { option: "C", text: "Italy", correct: false },
+      { option: "D", text: "Spain", correct: false },
+    ],
+  },
+  {
+    quiz: "Tokyo is the capital of ",
+    options: [
+      { option: "A", text: "China", correct: false },
+      { option: "B", text: "Japan", correct: true },
+      { option: "C", text: "South Korea", correct: false },
+      { option: "D", text: "India", correct: false },
+    ],
+  },
+  {
+    quiz: "Kuala Lumpur is the capital of ",
+    options: [
+      { option: "A", text: "Vietnam", correct: false },
+      { option: "B", text: "Malaysia", correct: true },
+      { option: "C", text: "Sweden", correct: false },
+      { option: "D", text: "Austria", correct: false },
+    ],
+  },
+  {
+    quiz: "The Eiffel Tower is located in ",
+    options: [
+      { option: "A", text: "Germany", correct: false },
+      { option: "B", text: "Italy", correct: false },
+      { option: "C", text: "France", correct: true },
+      { option: "D", text: "Spain", correct: false },
+    ],
+  },
+  {
+    quiz: "The Sydney Opera House is a famous landmark in ",
+    options: [
+      { option: "A", text: "Australia", correct: true },
+      { option: "B", text: "New Zealand", correct: false },
+      { option: "C", text: "South Africa", correct: false },
+      { option: "D", text: "Canada", correct: false },
+    ],
+  },
+];

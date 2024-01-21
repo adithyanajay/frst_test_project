a52a777f166b90c4cce14c949da4e4b6313f526d
adithyanajay@gmail.com <adithyanajay@gmail.com>
2024-01-22 00:27:00 +0530

    starter files

--- src/components/Playground/Playground.jsx
+++ src/components/Playground/Playground.jsx
@@ -1,13 +1,9 @@
 import "./Playground.css";
 
-function Playground({ component }) {
+function Playground() {
   return (
-    <div className="Playground">
-      <h1 className="Playground-heading">country quiz</h1>
-      <div className="MainMenu-content-sec">
-        <component />
-      </div>
-    </div>
+    //Write your code here
+    <div></div>
   );
 }
 

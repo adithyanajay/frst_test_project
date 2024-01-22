4466dbdc6a8f947f80db298e965d2efc49867e88
adithyanajay@gmail.com <adithyanajay@gmail.com>
2024-01-22 18:19:43 +0530

    typo fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import "./App.css";
-
 // Write your code here..............
 
+import "./App.css";
+import Playground from "./components/Playground/Playground";
+import WelcomeScreen from "./pages/WelcomeScreen/WelcomeScreen";
+import QuizScreen from "./pages/QuizScreen/QuizScreen";
+import ResultScreen from "./pages/ResultScreen/ResultScreen";
+import { Screen } from "./context/index";
+import { useState } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const screens = [WelcomeScreen, QuizScreen, ResultScreen];
+  const [currentScreen, setCurrentScreen] = useState(0);
+  return (
+    <div className="App">
+      <Screen.Provider value={{ currentScreen, setCurrentScreen }}>
+        <Playground Component={screens[currentScreen]} />
+      </Screen.Provider>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/WelcomeScreen/WelcomeScreen.jsx
+++ src/pages/WelcomeScreen/WelcomeScreen.jsx
@@ -1,7 +1,13 @@
+import MediumBtn from "../../components/MediumBtn/MediumBtn";
 import "./WelcomeScreen.css";
 
 function WelcomeScreen() {
-  return <div className="WelcomeScreen">Welcome Screen</div>;
+  return (
+    <div className="WelcomeScreen">
+      <h1>Welcome Screen</h1>
+      <MediumBtn />
+    </div>
+  );
 }
 
 export default WelcomeScreen;

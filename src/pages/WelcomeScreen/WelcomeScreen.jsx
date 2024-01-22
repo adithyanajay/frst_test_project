271c05a4e953fff4206952c4ed9627ab9a42efeb
adithyanajay@gmail.com <adithyanajay@gmail.com>
2024-01-22 20:23:20 +0530

    Welcome Screen editied

--- src/pages/WelcomeScreen/WelcomeScreen.jsx
+++ src/pages/WelcomeScreen/WelcomeScreen.jsx
@@ -1,11 +1,18 @@
 import MediumBtn from "../../components/MediumBtn/MediumBtn";
 import "./WelcomeScreen.css";
 
+export let i = 0;
 function WelcomeScreen() {
   return (
     <div className="WelcomeScreen">
       <h1>Welcome Screen</h1>
-      <MediumBtn text={"Start"} onClickFunc={() => console.log("test")} />
+      <MediumBtn
+        text={"Start"}
+        onClickFunc={() => {
+          i = 1;
+          console.log(i);
+        }}
+      />
     </div>
   );
 }

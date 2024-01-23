00077ccfdbbbdc07ae331b85bb854b6c6d04567c
adithyanajay@gmail.com <adithyanajay@gmail.com>
2024-01-23 11:16:42 +0530

    starter file for quizScreen

--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
 import "./Option.css";
-//Write your code here..........
-function Option() {
-  return <div className={`Option`}></div>;
+
+function Option({
+  optionLetter,
+  text,
+  correct,
+  userClicked,
+  setUserClicked,
+  setGameEnd,
+}) {
+  const [optionClicked, setOptionClicked] = useState(false);
+  useEffect(() => {
+    if (optionClicked && !correct) {
+      setGameEnd(true);
+    }
+  }, [optionClicked]);
+
+  const checkFunction = () => {
+    if (!userClicked) {
+      setUserClicked(true);
+      setOptionClicked(true);
+    }
+  };
+  return (
+    <div
+      className={`Option ${!userClicked ? "OptionClick" : ""}
+      ${userClicked && correct ? "OptionCorrect" : ""}
+      ${optionClicked && !correct ? "OptionWrong" : ""} 
+      `}
+      onClick={checkFunction}
+    >
+      <h2 className="OptionLetter">{optionLetter}</h2>
+      <p className="OptionText">{text}</p>
+    </div>
+  );
 }
 
 export default Option;
--- src/pages/QuizScreen/QuizScreen.jsx
+++ src/pages/QuizScreen/QuizScreen.jsx
@@ -1,46 +1,7 @@
-import { useEffect, useState } from "react";
-import Option from "../../components/Option/Option";
 import "./QuizScreen.css";
-
-export let i = false;
-export let j = false;
-
+//Write your code here............
 function QuizScreen() {
-  //Note this page is modified for testing purpose
-  //Don't modify this page, It can break your playground
-  //Only update code in src(src/components/Option/Option.jsx)
-  const [userClicked, setUserClicked] = useState(false);
-  const [gameEnd, setGameEnd] = useState(false);
-
-  useEffect(() => {
-    if (userClicked) {
-      i = true;
-    }
-    if (gameEnd) {
-      j = true;
-    }
-  }, [userClicked, gameEnd]);
-
-  return (
-    <div className="QuizScreen">
-      <Option
-        optionLetter={"A"}
-        text={"Correct"}
-        correct={true}
-        userClicked={userClicked}
-        setUserClicked={setUserClicked}
-        setGameEnd={setGameEnd}
-      />
-      <Option
-        optionLetter={"B"}
-        text={"Wrong"}
-        correct={false}
-        userClicked={userClicked}
-        setUserClicked={setUserClicked}
-        setGameEnd={setGameEnd}
-      />
-    </div>
-  );
+  return <div className="QuizScreen"></div>;
 }
 
 export default QuizScreen;

04c952b44cf4c22e77053740073703e18bf042e4
adithyanajay@gmail.com <adithyanajay@gmail.com>
2024-01-23 08:48:46 +0530

    WelcomeScreen updated

--- src/pages/WelcomeScreen/WelcomeScreen.jsx
+++ src/pages/WelcomeScreen/WelcomeScreen.jsx
@@ -1,8 +1,20 @@
 import "./WelcomeScreen.css";
-//Write your code here.................
+import MediumBtn from "../../components/MediumBtn/MediumBtn";
+import { Screen } from "../../context/index.js";
+import { useContext } from "react";
 
 function WelcomeScreen() {
-  return <div className="WelcomeScreen"></div>;
+  const { setCurrentScreen } = useContext(Screen);
+  return (
+    <div className="WelcomeScreen">
+      <h1 className="WelcomeScreen-heading">Welcome</h1>
+      <p className="WelcomeScreen-para">
+        Discover the world, one question at a time – welcome to the Country Quiz
+        Challenge!
+      </p>
+      <MediumBtn text={"Start"} onClickFunc={() => setCurrentScreen(1)} />
+    </div>
+  );
 }
 
 export default WelcomeScreen;

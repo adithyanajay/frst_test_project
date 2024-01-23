8bf66e2bdaa8f3c746e57fc110040dc7df41762a
adithyanajay@gmail.com <adithyanajay@gmail.com>
2024-01-23 18:11:28 +0530

    Minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { useState } from "react";
 function App() {
   const screens = [WelcomeScreen, QuizScreen, ResultScreen];
   const [resultCount, setResultCount] = useState(0);
-  const [currentScreen, setCurrentScreen] = useState(1);
+  const [currentScreen, setCurrentScreen] = useState(2);
   return (
     <div className="App">
       <Result.Provider value={{ resultCount, setResultCount }}>

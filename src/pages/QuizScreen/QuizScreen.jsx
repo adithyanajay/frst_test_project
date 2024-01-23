c37ccaeaad036cca294a99b60b1c8b991d77ee76
adithyanajay@gmail.com <adithyanajay@gmail.com>
2024-01-23 14:09:03 +0530

    starter files added

--- src/pages/QuizScreen/QuizScreen.jsx
+++ src/pages/QuizScreen/QuizScreen.jsx
@@ -1,4 +1,10 @@
 import "./QuizScreen.css";
+import illustration from "../../assets/illustration.svg";
+import { useContext, useState } from "react";
+import { quizzes } from "../../quizzes";
+import QuizOption from "../../components/QuizOption/QuizOption";
+import SmallBtn from "../../components/SmallBtn/SmallBtn";
+
 //Write your code here............
 function QuizScreen() {
   return <div className="QuizScreen"></div>;
